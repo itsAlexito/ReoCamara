7474ce782101da2e69c56527fb1ae959270df865
itsalexito <yodorte.ab2@gmail.com>
2025-02-26 14:37:44 +0100

    Fix bug

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 from telegram.ext import Application, CommandHandler, MessageHandler, filters
-from config import BOT_TOKEN, ALLOWED_CHAT_IDS
+from config import BOT_TOKEN, ALLOWED_CHAT_IDS, GODMODE
 from handlers import start_route, unknown_command, get_image, get_video, not_allowed_reply, forward_message
 
 def main():
     application = Application.builder().token(BOT_TOKEN).build()
+    
+    # Filtros de permisos
     allowed_filter = filters.Chat(chat_id=ALLOWED_CHAT_IDS)
-    disallowed_filter = ~allowed_filter
+    
+    #GODMODE filter
+    godmode_filter = filters.User(user_id=GODMODE)
+    
+    # Bloquear comandos en chats no autorizados, excepto si el usuario está en GODMODE
+    disallowed_filter = ~filters.Chat(chat_id=ALLOWED_CHAT_IDS) & ~godmode_filter
 
     # Agregar handlers para comandos permitidos
-    application.add_handler(CommandHandler("getsalseo", start_route, filters=allowed_filter))
-    application.add_handler(CommandHandler("getnevera", start_route, filters=allowed_filter))
-    application.add_handler(CommandHandler("getimage", get_image, filters=allowed_filter))
-    application.add_handler(CommandHandler("getvideo", get_video, filters=allowed_filter))
-    application.add_handler(CommandHandler("forward", forward_message, filters=allowed_filter.ChatType.is_private))
+    application.add_handler(CommandHandler("getsalseo", start_route, filters=allowed_filter | godmode_filter))
+    application.add_handler(CommandHandler("getnevera", start_route, filters=allowed_filter | godmode_filter))
+    application.add_handler(CommandHandler("getimage", get_image, filters=allowed_filter | godmode_filter))
+    application.add_handler(CommandHandler("getvideo", get_video, filters=allowed_filter | godmode_filter))
+    application.add_handler(CommandHandler("say", forward_message, filters=filters.ChatType.PRIVATE & godmode_filter))
 
-    # Manejar comandos de chats no permitidos
+    # Manejar comandos de chats no permitidos (excepto GODMODE)
     application.add_handler(MessageHandler(filters.COMMAND & disallowed_filter, not_allowed_reply))
-    # Para comandos no reconocidos en chats permitidos
-    application.add_handler(MessageHandler(filters.COMMAND & allowed_filter, unknown_command))
+
+    # Para comandos no reconocidos en chats permitidos o por usuarios en GODMODE
+    application.add_handler(MessageHandler(filters.COMMAND & (allowed_filter | godmode_filter), unknown_command))
 
     application.run_polling()
 
